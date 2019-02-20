171ecfb2266f5b7eae11b72a24fe0de19c0744ab
clementlefevre <clement.san@gmail.com>
2019-02-20 11:21:01 +0100

    fix send email

--- service/send_email.py
+++ service/send_email.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 import os
@@ -11,40 +10,35 @@ from email.mime.base import MIMEBase
 from email.encoders import encode_base64
 
 import logging
+from config import SENDER_EMAIL, SENDER_PWD
+
 logger = logging.getLogger(__name__)
 
 
-from config import TARGET_EMAILS,SENDER_EMAIL,SENDER_PWD
+def send_notification(new_offers_list, query):
 
-
-
-
-def send_notification(new_offers_list,excel_file):
-    
-    
     # Compose attachment
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload(open(excel_file, "rb").read())
-    encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= %s" % os.path.basename(excel_file))
+    # part = MIMEBase('application', "octet-stream")
+    # part.set_payload(open(excel_file, "rb").read())
+    # encode_base64(part)
+    # part.add_header('Content-Disposition', "attachment; filename= %s" % os.path.basename(excel_file))
 
     # Compose message
     msg = MIMEMultipart()
-    msg['Subject']="Degewo : {} new offers".format(len(new_offers_list))
-    msg['From'] = SENDER_EMAIL
-    msg['To'] = ','.join(TARGET_EMAILS)
-    msg.attach(MIMEText('\n'.join(new_offers_list), 'plain'))
-    msg.attach(part)
+    msg["Subject"] = "Degewo : {} new offers".format(len(new_offers_list))
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = ",".join(query.receiver)
+    msg.attach(MIMEText("\n".join(new_offers_list), "plain"))
+    # msg.attach(part)
 
-    try:  
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    try:
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
         server.ehlo()
         server.login(SENDER_EMAIL, SENDER_PWD)
-        server.sendmail(SENDER_EMAIL, TARGET_EMAILS, msg.as_string())
+        server.sendmail(SENDER_EMAIL, query.receiver, msg.as_string())
         server.close()
-        logging.debug('Email sent!')
-    except Exception as e:  
-        
-        logging.error('Something went wrong...')
+        logging.debug("Email sent!")
+    except Exception as e:
+
+        logging.error("Something went wrong...")
         logging.error(e)
- 
